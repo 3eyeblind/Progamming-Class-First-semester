a61c68f4a6a70c572f717f4f288833f1e5e80e5b
3eyeblind <logan.greenawalt@icloud.com>
2024-08-31 12:56:03 -0400

    hello and checkpoint

---
+++ checkpoint2.14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    char firstInitial;
+    char middleInitial;
+    char lastInitial;
+    std::string firstName;
+    std::cout << "Please enter the initial of your first name:";
+    std::cin >> firstInitial;
+    std::cout << "Please enter the initial of your middle name:";
+    std::cin >> middleInitial;
+    std::cout << "Please enter the initial of your last name:";
+    std::cin >> lastInitial;
+    std::cout << "Your initals are: " << firstInitial << middleInitial << lastInitial << std::endl;
+    std::cout << "Please enter your first name:";
+    std::cin >> firstName;
+    std::cout << "Your first name is : " << firstName;
+}
