ea84790ed5acb939960adc8e69c3fea05abff36c
3eyeblind <logan.greenawalt@icloud.com>
2024-09-26 20:44:59 -0400

    ocean main fix

--- ocean.cpp
+++ ocean.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int main()
 {
-    const double riseRate = 1.5; // Ocean level rise rate in millimeters per year
-    int years[] = {5, 7, 10};    // Years for which we will calculate the rise
+    const double riseRate = 1.5;
+    int years[] = {5, 7, 10};
 
-    // Output the ocean level rise for specified years
     for (int i = 0; i < 3; i++)
     {
         cout << "In " << years[i] << " years, the ocean level will be "
