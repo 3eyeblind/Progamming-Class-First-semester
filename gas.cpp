83423e9d8984640b084bc7a5c65ce6acfded3a47
3eyeblind <logan.greenawalt@icloud.com>
2024-09-26 21:54:00 -0400

    gas main

---
+++ gas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    double miles = 375;
+    double gallons = 15;
+
+    double mpg = miles / gallons;
+
+    cout << "The car gets " << mpg << " miles per gallon." << endl;
+
+    return 0;
+}
