e8b690928ad455f24a8a571459ff4d8734a7b65b
3eyeblind <logan.greenawalt@icloud.com>
2024-11-12 23:37:46 -0500

    both guess games

--- guess-game/guess-game.cpp
+++ guess-game/guess-game.cpp
@@ -3,13 +3,14 @@
  * Description: This program generates a random number between 1 and 100 and asks the user
  *              to guess what it is. It provides feedback on whether the user's guess is
  *              too high or too low. The program continues prompting the user until they
- *              correctly guess the random number.
- *              The program makes use of a loop that repeats until the user guesses correctly.
+ *              correctly guess the random number. The program also keeps track of the number
+ *              of guesses and displays the total number of guesses when the correct number is guessed.
  *              - If the user's guess is higher than the random number, the program displays
  *                "Too high, try again."
  *              - If the user's guess is lower than the random number, the program displays
  *                "Too low, try again."
- *              - When the correct number is guessed, the program congratulates the user.
+ *              - When the correct number is guessed, the program congratulates the user and
+ *                displays the number of guesses made.
  */
 
 #include <iostream>
@@ -20,11 +21,11 @@ using namespace std;
 
 int main()
 {
-
     srand(static_cast<unsigned int>(time(0)));
 
     int randomNumber = rand() % 100 + 1;
     int guess = 0;
+    int guessCount = 0;
 
     cout << "Welcome to the Random Number Guessing Game!" << endl;
     cout << "I have generated a random number between 1 and 100." << endl;
@@ -32,9 +33,10 @@ int main()
 
     while (guess != randomNumber)
     {
-
         cout << "Enter your guess: ";
         cin >> guess;
+
+        guessCount++;
 
         if (guess > randomNumber)
         {
@@ -46,8 +48,8 @@ int main()
         }
         else
         {
-
             cout << "Congratulations! You've guessed the right number!" << endl;
+            cout << "It took you " << guessCount << " guesses." << endl;
         }
     }
 
