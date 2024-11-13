081ca96ed23eca0409e10a7c7a32a003d2369bef
3eyeblind <logan.greenawalt@icloud.com>
2024-11-12 23:23:23 -0500

    guess game

---
+++ guess-game/guess-game.cpp
@@ -0,0 +1,55 @@
+/*
+ * Program: Random Number Guessing Game
+ * Description: This program generates a random number between 1 and 100 and asks the user
+ *              to guess what it is. It provides feedback on whether the user's guess is
+ *              too high or too low. The program continues prompting the user until they
+ *              correctly guess the random number.
+ *              The program makes use of a loop that repeats until the user guesses correctly.
+ *              - If the user's guess is higher than the random number, the program displays
+ *                "Too high, try again."
+ *              - If the user's guess is lower than the random number, the program displays
+ *                "Too low, try again."
+ *              - When the correct number is guessed, the program congratulates the user.
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+
+    srand(static_cast<unsigned int>(time(0)));
+
+    int randomNumber = rand() % 100 + 1;
+    int guess = 0;
+
+    cout << "Welcome to the Random Number Guessing Game!" << endl;
+    cout << "I have generated a random number between 1 and 100." << endl;
+    cout << "Can you guess what it is?" << endl;
+
+    while (guess != randomNumber)
+    {
+
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess > randomNumber)
+        {
+            cout << "Too high, try again." << endl;
+        }
+        else if (guess < randomNumber)
+        {
+            cout << "Too low, try again." << endl;
+        }
+        else
+        {
+
+            cout << "Congratulations! You've guessed the right number!" << endl;
+        }
+    }
+
+    return 0;
+}
